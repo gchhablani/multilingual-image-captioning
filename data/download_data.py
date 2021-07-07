56428182a1d52e561c75752ca0cf72bd16cd72ed
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-07-08 03:06:08 +0530

    Fix style

--- data/download_data.py
+++ data/download_data.py
@@ -1,32 +1,37 @@
-'''
+"""
 This script was adapted from https://github.com/igorbrigadir/DownloadConceptualCaptions
 Few changes were made post that (excluding the post processing of data). We'll have
 only csv file with image url and captions written in different languages but not images
 as we do not own any of the images in the dataset and hence cannot legally provide them to you.
-'''
-import pandas as pd
-import numpy as np
-import requests
-import zlib
+"""
 import os
 import shelve
-import magic
+import zlib
 from multiprocessing import Pool
+
+import magic
+import numpy as np
+import pandas as pd
+import requests
 from tqdm import tqdm
 
 headers = {
-    'User-Agent':'Googlebot-Image/1.0', # Pretend to be googlebot
-    'X-Forwarded-For': '64.18.15.200'
+    "User-Agent": "Googlebot-Image/1.0",  # Pretend to be googlebot
+    "X-Forwarded-For": "64.18.15.200",
 }
+
 
 def _df_split_apply(tup_arg):
     split_ind, subset, func = tup_arg
     r = subset.apply(func, axis=1)
     return (split_ind, r)
 
+
 def df_multiprocess(df, processes, chunk_size, func, dataset_name):
     print("Generating parts...")
-    with shelve.open('%s_%s_%s_results.tmp' % (dataset_name, func.__name__, chunk_size)) as results:
+    with shelve.open(
+        "%s_%s_%s_results.tmp" % (dataset_name, func.__name__, chunk_size)
+    ) as results:
 
         pbar = tqdm(total=len(df), position=0)
         # Resume:
@@ -35,12 +40,26 @@ def df_multiprocess(df, processes, chunk_size, func, dataset_name):
         for k in results.keys():
             pbar.update(len(results[str(k)][1]))
 
-        pool_data = ((index, df[i:i + chunk_size], func) for index, i in enumerate(range(0, len(df), chunk_size)) if index not in finished_chunks)
-        print(int(len(df) / chunk_size), "parts.", chunk_size, "per part.", "Using", processes, "processes")
+        pool_data = (
+            (index, df[i : i + chunk_size], func)
+            for index, i in enumerate(range(0, len(df), chunk_size))
+            if index not in finished_chunks
+        )
+        print(
+            int(len(df) / chunk_size),
+            "parts.",
+            chunk_size,
+            "per part.",
+            "Using",
+            processes,
+            "processes",
+        )
 
         pbar.desc = "Downloading"
         with Pool(processes) as pool:
-            for i, result in enumerate(pool.imap_unordered(_df_split_apply, pool_data, 2)):
+            for i, result in enumerate(
+                pool.imap_unordered(_df_split_apply, pool_data, 2)
+            ):
                 results[str(result[0])] = result
                 pbar.update(len(result[1]))
         pbar.close()
@@ -48,16 +67,23 @@ def df_multiprocess(df, processes, chunk_size, func, dataset_name):
     print("Finished Downloading.")
     return
 
+
 # Unique name based on url
 def _file_name(row):
-    return "%s/%s_%s" % (row['folder'], row.name, (zlib.crc32(row['url'].encode('utf-8')) & 0xffffffff))
+    return "%s/%s_%s" % (
+        row["folder"],
+        row.name,
+        (zlib.crc32(row["url"].encode("utf-8")) & 0xFFFFFFFF),
+    )
+
 
 # For checking mimetypes separately without download
 def check_mimetype(row):
-    if os.path.isfile(str(row['file'])):
-        row['mimetype'] = magic.from_file(row['file'], mime=True)
-        row['size'] = os.stat(row['file']).st_size
+    if os.path.isfile(str(row["file"])):
+        row["mimetype"] = magic.from_file(row["file"], mime=True)
+        row["size"] = os.stat(row["file"]).st_size
     return row
+
 
 # Don't download image, just check with a HEAD request, can't resume.
 # Can use this instead of download_image to get HTTP status codes.
@@ -65,88 +91,126 @@ def check_download(row):
     fname = _file_name(row)
     try:
         # not all sites will support HEAD
-        response = requests.head(row['url'], stream=False, timeout=5, allow_redirects=True, headers=headers)
-        row['status'] = response.status_code
-        row['headers'] = dict(response.headers)
+        response = requests.head(
+            row["url"], stream=False, timeout=5, allow_redirects=True, headers=headers
+        )
+        row["status"] = response.status_code
+        row["headers"] = dict(response.headers)
     except:
         # log errors later, set error as 408 timeout
-        row['status'] = 408
+        row["status"] = 408
         return row
     if response.ok:
-        row['file'] = fname
+        row["file"] = fname
     return row
+
 
 def download_image(row):
     fname = _file_name(row)
     # Skip Already downloaded, retry others later
     if os.path.isfile(fname):
-        row['status'] = 200
-        row['file'] = fname
-        row['mimetype'] = magic.from_file(row['file'], mime=True)
-        row['size'] = os.stat(row['file']).st_size
+        row["status"] = 200
+        row["file"] = fname
+        row["mimetype"] = magic.from_file(row["file"], mime=True)
+        row["size"] = os.stat(row["file"]).st_size
         return row
 
     try:
         # use smaller timeout to skip errors, but can result in failed downloads
-        response = requests.get(row['url'], stream=False, timeout=10, allow_redirects=True, headers=headers)
-        row['status'] = response.status_code
-        #row['headers'] = dict(response.headers)
+        response = requests.get(
+            row["url"], stream=False, timeout=10, allow_redirects=True, headers=headers
+        )
+        row["status"] = response.status_code
+        # row['headers'] = dict(response.headers)
     except Exception as e:
         # log errors later, set error as 408 timeout
-        row['status'] = 408
+        row["status"] = 408
         return row
 
     if response.ok:
         try:
-            with open(fname, 'wb') as out_file:
+            with open(fname, "wb") as out_file:
                 # some sites respond with gzip transport encoding
                 response.raw.decode_content = True
                 out_file.write(response.content)
-            row['mimetype'] = magic.from_file(fname, mime=True)
-            row['size'] = os.stat(fname).st_size
+            row["mimetype"] = magic.from_file(fname, mime=True)
+            row["size"] = os.stat(fname).st_size
         except:
             # This is if it times out during a download or decode
-            row['status'] = 408
+            row["status"] = 408
             return row
-        row['file'] = fname
+        row["file"] = fname
     return row
+
 
 def open_tsv(fname, folder):
     print("Opening %s Data File..." % fname)
-    df = pd.read_csv(fname, sep='\t', names=["caption","url"], usecols=range(1,2))
-    df['folder'] = folder
+    df = pd.read_csv(fname, sep="\t", names=["caption", "url"], usecols=range(1, 2))
+    df["folder"] = folder
     print("Processing", len(df), " Images:")
     return df
 
+
 def df_from_shelve(chunk_size, func, dataset_name):
     print("Generating Dataframe from results...")
-    with shelve.open('%s_%s_%s_results.tmp' % (dataset_name, func.__name__, chunk_size)) as results:
+    with shelve.open(
+        "%s_%s_%s_results.tmp" % (dataset_name, func.__name__, chunk_size)
+    ) as results:
         keylist = sorted([int(k) for k in results.keys()])
         df = pd.concat([results[str(k)][1] for k in keylist], sort=True)
     return df
+
 
 # number of processes in the pool can be larger than cores
 num_processes = 256
 # chunk_size is how many images per chunk per process - changing this resets progress when restarting.
 images_per_part = 200
 
-'''
+"""
 A bunch of them will fail to download, and return web pages instead. These will
 need to be cleaned up later. See downloaded_validation_report.tsv after it downloads
 for HTTP errors. Around 10-11% of images are gone, based on validation set results. Setting
 the user agent could fix some errors too maybe - not sure if any requests are rejected by
 sites based on this.
-'''
+"""
 data_name = "validation"
 df = open_tsv("Validation_GCC-1.1.0-Validation.tsv", data_name)
-df_multiprocess(df=df, processes=num_processes, chunk_size=images_per_part, func=download_image, dataset_name=data_name)
-df = df_from_shelve(chunk_size=images_per_part, func=download_image, dataset_name=data_name)
-df.to_csv("downloaded_%s_report.tsv.gz" % data_name, compression='gzip', sep='\t', header=False, index=False)
+df_multiprocess(
+    df=df,
+    processes=num_processes,
+    chunk_size=images_per_part,
+    func=download_image,
+    dataset_name=data_name,
+)
+df = df_from_shelve(
+    chunk_size=images_per_part, func=download_image, dataset_name=data_name
+)
+df.to_csv(
+    "downloaded_%s_report.tsv.gz" % data_name,
+    compression="gzip",
+    sep="\t",
+    header=False,
+    index=False,
+)
 print("Saved.")
 
 data_name = "training"
-df = open_tsv("Train_GCC-training.tsv",data_name)
-df_multiprocess(df=df, processes=num_processes, chunk_size=images_per_part, func=download_image, dataset_name=data_name)
-df = df_from_shelve(chunk_size=images_per_part, func=download_image, dataset_name=data_name)
-df.to_csv("downloaded_%s_report.tsv.gz" % data_name, compression='gzip', sep='\t', header=False, index=False)
+df = open_tsv("Train_GCC-training.tsv", data_name)
+df_multiprocess(
+    df=df,
+    processes=num_processes,
+    chunk_size=images_per_part,
+    func=download_image,
+    dataset_name=data_name,
+)
+df = df_from_shelve(
+    chunk_size=images_per_part, func=download_image, dataset_name=data_name
+)
+df.to_csv(
+    "downloaded_%s_report.tsv.gz" % data_name,
+    compression="gzip",
+    sep="\t",
+    header=False,
+    index=False,
+)
 print("Saved.")
--- data/translate_cc_captions.py
+++ data/translate_cc_captions.py
@@ -1,38 +1,61 @@
-import pandas as pd
+import argparse
+import json
+import multiprocessing
 import os
 import urllib.request
-import json
-from tqdm.auto import tqdm
-import mtranslate
 
-import argparse
-import multiprocessing
+import mtranslate
+import pandas as pd
+from tqdm.auto import tqdm
 
 
 def read_tsv_file(tsv_path):
-	df = pd.read_csv(tsv_path, delimiter="\t", header=None)
-	print("Number of Examples:", df.shape[0], "for", tsv_path)
-	print(df.head())
-	return df
+    df = pd.read_csv(tsv_path, delimiter="\t", header=None)
+    print("Number of Examples:", df.shape[0], "for", tsv_path)
+    print(df.head())
+    return df
+
 
 def arrange_data(caption, image_url):
-	# print(caption, image_url)
-	try:
-		return {"captions":{**{"en":caption},**{lang: mtranslate.translate(caption, lang, "en") for lang in LANG_LIST}}, "url":image_url}
-	except Exception as e:
-		print(caption, image_url, " skipped!")
-		return
+    # print(caption, image_url)
+    try:
+        return {
+            "captions": {
+                **{"en": caption},
+                **{
+                    lang: mtranslate.translate(caption, lang, "en")
+                    for lang in LANG_LIST
+                },
+            },
+            "url": image_url,
+        }
+    except Exception as e:
+        print(caption, image_url, " skipped!")
+        return
 
-def arrange_data_mp(df, number_required = 1000):
-	pool = multiprocessing.Pool(os.cpu_count())    
-	path_wise_data = pool.starmap(arrange_data, zip(list(df[0]), list(df[1])))
-	return path_wise_data
+
+def arrange_data_mp(df, number_required=1000):
+    pool = multiprocessing.Pool(os.cpu_count())
+    path_wise_data = pool.starmap(arrange_data, zip(list(df[0]), list(df[1])))
+    return path_wise_data
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset_store_path", type=str, default="conceptual_captions", help="path of directory where the dataset is to be saved")
-parser.add_argument("--no_of_train_samples", type=int, default=1000, help="Truncation size of Train Subset")
-parser.add_argument("--no_of_val_samples", type=int, default=100, help="Truncation size of Val Subset")
+parser.add_argument(
+    "--dataset_store_path",
+    type=str,
+    default="conceptual_captions",
+    help="path of directory where the dataset is to be saved",
+)
+parser.add_argument(
+    "--no_of_train_samples",
+    type=int,
+    default=1000,
+    help="Truncation size of Train Subset",
+)
+parser.add_argument(
+    "--no_of_val_samples", type=int, default=100, help="Truncation size of Val Subset"
+)
 parser.add_argument("--lang_list", nargs="+", default=["fr"])
 args = parser.parse_args()
 
@@ -64,7 +87,7 @@ train_json = arrange_data_mp(train_df, train_save_dir)
 val_json = arrange_data_mp(val_df, val_save_dir)
 
 with open(train_save_dir, "w") as f:
-	json.dump(train_json, f, indent=4)
+    json.dump(train_json, f, indent=4)
 
 with open(val_save_dir, "w") as f:
-	json.dump(val_json, f, indent=4)
+    json.dump(val_json, f, indent=4)
