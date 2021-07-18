474f41c56da91c01833a136c533b31b4c70e80e9
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2021-07-18 13:12:14 +0530

    update bash

--- run.sh
+++ run.sh
@@ -11,25 +11,23 @@ fi
 
 token=$(cat /home/bhavitvya_malik/.huggingface/token)
 
+
 echo "Token found $token"
 python3 main.py \
     --output_dir $repo \
     --seed 42 \
-    --logging_steps 5000 \
-    --eval_steps 10000 \
-    --save_steps 15000 \
+    --logging_steps 500 \
+    --eval_steps 1000 \
+    --save_steps 2500 \
     --data_dir /home/user/data/CC12M/images \
-    --train_file /home/bhavitvya_malik/final_data/train_file_batch.tsv \
-    --validation_file /home/bhavitvya_malik/final_data/val_file_batch.tsv \
-    --save_total_limit 10 \
+    --train_file /home/bhavitvya_malik/final_data/subset_data/train_file_batch.tsv \
+    --validation_file /home/bhavitvya_malik/final_data/subset_data/val_file_batch.tsv \
+    --save_total_limit 6 \
     --push_to_hub \
-    --num_train_epochs 3 \
+    --num_train_epochs 5 \
     --push_to_hub_organization flax-community \
     --push_to_hub_token $token \
-    --max_eval_samples_per_lang 25000 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 64 \
     --per_device_eval_batch_size 64 \
-    --preprocessing_num_workers 8 \
-    --warmup_steps 10000 \
-    --learning_rate 1e-4 \
-    --predict_with_generate False \
+    --preprocessing_num_workers 16 \
+    --warmup_steps 1000 \
